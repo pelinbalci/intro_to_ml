574c35cbedf8958f8bd35f2d750e9b91a133e068
Pelin Balci <balci.pelin@gmail.com>
2020-07-08 11:16:39 +0300

    add centroids to plot

--- K_means/k_means_manual.py
+++ K_means/k_means_manual.py
@@ -1,8 +1,9 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import random
-from collections import defaultdict
+
 random.seed(42)
+
 
 # create data
 def addData(data, num_instance, lowx, lowy, highx, highy):
@@ -11,20 +12,15 @@ def addData(data, num_instance, lowx, lowy, highx, highy):
     y = random.randint(lowy, highy)
     data.append([x,y])
 
-
-data = []
-# data = [[2,14], [12,16], ..., []]
+data = []  # data = [[2,14], [12,16], ..., []]
 num_instance = 100
 addData(data, num_instance, 0, 0, 20, 20)
 addData(data, num_instance, 40, 0, 60, 20)
 addData(data, num_instance, 20, 50, 40, 70)
 
 # x and y values in data
-x_values = []
-y_values = []
-for d in data:
-    x_values.append(d[0])
-    y_values.append(d[1])
+x_values = [item[0] for item in data]
+y_values = [item[1] for item in data]
 
 # random centers
 k = 3
@@ -34,23 +30,18 @@ for i in range(k):
   random_idx = random.randint(0, len(data)-1)
   centroids.append(data[random_idx])
 
-cent_x = []
-cent_y = []
-for d in centroids:
-    cent_x.append(d[0])
-    cent_y.append(d[1])
+cent_x = [item[0] for item in centroids]
+cent_y = [item[1] for item in centroids]
 
-#plot data and random centers
-plt.plot(x_values, y_values, "ro")
-plt.plot(cent_x, cent_y, "bo")
+# plot data and random centers
+plt.plot(x_values, y_values, "yo")
+plt.plot(cent_x, cent_y, "go")
 plt.show()
 
-print('done')
 
-# find distances
+# find clusters
 max_iter = 300
 cluster_num = None
-
 for iter in range(max_iter):
     cluster = {0: [], 1: [], 2: []}
     for i in range(len(x_values)):
@@ -72,12 +63,12 @@ for iter in range(max_iter):
         cent_x.append(sum(assigned_x) / len(assigned_x))
         cent_y.append(sum(assigned_y) / len(assigned_y))
 
+# final plot
 plt.figure()
-colors = ["red", "green", "blue"]
+colors = ["red", "yellow", "blue"]
 for j in range(k):
     assigned_x = [item[0] for item in cluster[j]]
     assigned_y = [item[1] for item in cluster[j]]
     plt.plot(assigned_x, assigned_y, "o", color=colors[j])
+plt.plot(cent_x, cent_y, "go")
 plt.show()
-
-print('done')
